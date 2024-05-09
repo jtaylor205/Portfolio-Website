02ca3a2d5e464d25dd5051a8476a708a13ed97a5
Jaedon Taylor <jaedonataylor@gmail.com>
2024-05-09 17:49:56 -0400

    worked on slideover nav. Added animation to the items

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Hamburger = ({ menuOpen, setMenuOpen, className = '' }) => {
+    const [isHovering, setIsHovering] = useState(false);
+
+    const handleMenuToggle = () => {
+        setMenuOpen(!menuOpen);
+        setIsHovering(false); 
+    };
+  
+  const handleMouseEnter = () => setIsHovering(true);
+  const handleMouseLeave = () => setIsHovering(false);
+
   return (
-    <div className={`hamburger ${menuOpen ? 'open' : ''} ${className}`} onClick={() => setMenuOpen(!menuOpen)}>
+    <div className={`hamburger ${menuOpen ? 'open' : ''} ${className} ${isHovering ? 'hover' : ''}`} onClick={handleMenuToggle}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}>
       <span></span>
       <span></span>
       <span></span>
--- src/components/SlideoverNav.jsx
+++ src/components/SlideoverNav.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
-import Hamburger from './Hamburger';
+import React, { useState } from 'react';
 
 const SlideoverNav = ({ menuOpen, setMenuOpen }) => {
+  const [navHoverIndex, setNavHoverIndex] = useState(-1);
+  const handleNavMouseEnter = (index) => setNavHoverIndex(index);
+  const handleNavMouseLeave = () => setNavHoverIndex(-1);
+
+  const [linkHoverIndex, setLinkHoverIndex] = useState(-1);
+  const handleLinkMouseEnter = (index) => setLinkHoverIndex(index);
+  const handleLinkMouseLeave = () => setLinkHoverIndex(-1);
+
+
   const navItems = [
     { title: "HOME" },
     { title: "ABOUT" },
     { title: "PROJECTS" },
   ];
 
+  const linkItems = [
+    {title: "LINKEDIN", link: 'https://www.linkedin.com/in/jaedon-taylor-982316102/'},
+    {title: "GITHUB", link: 'https://github.com/jtaylor205'},
+    {title: "RESUME"},
+  ];
+
   return (
     <div className={`slideover-container ${menuOpen ? 'open' : ''}`}>
-      <div className="hamburger-right">
-        <Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen} className="hamburger-slideover" />
-      </div>
-      <div className="nav-list">
+      <div className="slideover-tabs-container">
         {navItems.map((item, index) => (
-          <div key={index} className="nav-item">
-            {item.title}
+          <div key={index}>
+            <div
+              className={`slideover-tab ${navHoverIndex === index ? 'hover' : ''} ${index === 0 && navHoverIndex === 0 ? 'first-hover' : ''}`}
+              onMouseEnter={() => handleNavMouseEnter(index)}
+              onMouseLeave={handleNavMouseLeave}
+            >
+              {item.title}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="slideover-links-container">
+        {linkItems.map((item, index) => (
+          <div key={index}>
+            <div
+              className={`slideover-link ${linkHoverIndex === index ? 'hover' : ''}`}
+              onMouseEnter={() => handleLinkMouseEnter(index)}
+              onMouseLeave={handleLinkMouseLeave}
+            >
+              {item.title}
+            </div>
           </div>
         ))}
       </div>
