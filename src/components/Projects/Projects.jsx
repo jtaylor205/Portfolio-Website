3466fdf3a609d1de4286ffbe823c658a55d9bcab
Jaedon Taylor <jaedonataylor@gmail.com>
2024-08-29 21:49:47 -0400

    Improved Projects tab. Included formatting for mobile screenshots, and readjusted the grid layout

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,15 @@ import mineSweepeerImg1 from '../../assets/minesweeper1.png';
 import mineSweepeerImg2 from '../../assets/minesweeper2.png';
 import sudokuImg1 from '../../assets/sudoku1.png';
 import sudokuImg2 from '../../assets/sudoku2.png';
-import { SiCoursera, SiCplusplus, SiCss3, SiGooglegemini, SiHtml5, SiJavascript, SiOpenai, SiPython, SiReact, SiSwift, SiNumpy, SiGithub } from "react-icons/si";
+import fridgeImg1 from '../../assets/foodFridge1.png';
+import fridgeImg2 from '../../assets/foodFridge2.png';
+import fridgeImg3 from '../../assets/foodFridge3.png';
+import guruImg1 from '../../assets/phoneGuru1.png';
+import guruImg2 from '../../assets/phoneGuru2.png';
+import solaceImg1 from '../../assets/solace1.png';
+import solaceImg2 from '../../assets/solace2.png';
+import solaceImg3 from '../../assets/solace3.png';
+import { SiCoursera, SiCplusplus, SiCss3, SiGooglegemini, SiHtml5, SiJavascript, SiOpenai, SiPython, SiReact, SiSwift, SiNumpy, SiGithub, SiIcloud, SiFirebase, SiLinux, SiSfml} from "react-icons/si";
 
 const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
@@ -16,8 +24,10 @@ const Projects = () => {
       type: "Mobile App/AI",
       info: "Yes",
       link: "https://github.com/jtaylor205/solace",
+      screen: "Mobile",
+      images: [solaceImg1, solaceImg2, solaceImg3],
       bio: "Mental Wellness Application harnessing the power of generative AI to empower users in achieving their daily goals, maintaining a personal journal, and engaging in meaningful conversations with an AI therapist. This comprehensive platform integrates these essential mental health tools into one seamless experience, all wrapped in a beautifully designed and intuitive interface that promotes well-being and mindfulness.",
-      languages: <><SiReact /><SiJavascript /><SiGooglegemini/></>,
+      languages: <><SiReact /><SiJavascript /><SiGooglegemini/><SiFirebase/></>,
     }, 
     {
       title: "Minesweeper",
@@ -25,31 +35,29 @@ const Projects = () => {
       info: "Yes",
       link: "https://github.com/jtaylor205/minesweeper",
       images: [mineSweepeerImg1, mineSweepeerImg2],
+      screen: "Computer",
       bio: "A Minesweeper game developed in C++ using the SFML library, featuring a built-in timer and a local leaderboard to track and celebrate your best times. This classic puzzle game is enhanced with smooth graphics and responsive gameplay, offering an engaging experience for both casual and competitive players.",
-      languages: <SiCplusplus />,
+      languages: <><SiCplusplus /><SiSfml/></>,
     },
     {
       title: "Food Fridge",
       type: "Mobile App",
       info: "Yes",
       link: "https://github.com/jtaylor205/food-fridge",
+      images: [fridgeImg1, fridgeImg2, fridgeImg3],
+      screen: "Mobile",
       bio: "A SwiftUI application where users can efficiently manage their food inventory by storing items in a 'Fridge' and sorting them by food type. The app also enables users to create recipes using ingredients from their fridge and easily generate shopping lists for items needed for recipes or other purposes. It's a comprehensive tool designed to streamline meal planning and grocery shopping.",
-      languages: <SiSwift />,
+      languages: <><SiIcloud /><SiSwift /></>,
     },
     {
       title: "Phone Guru",
       type: "Web Development/AI",
       info: "Yes",
       link: "https://github.com/jtaylor205/verizon-hack",
+      images: [guruImg1, guruImg2],
+      screen: "Computer",
       bio: "A web-based application that assists users in finding the perfect phone based on their specific needs and preferences. Users can input the specifications they value most in a phone, and the AI-driven system analyzes mobile phone data to recommend the best possible phone matches for the user.",
       languages: <><SiPython /><SiHtml5/><SiCss3/><SiJavascript/><SiOpenai/></>,
-    },
-    {
-      title: "File System",
-      type: "Operating Systems",
-      info: "No",
-      bio: "A userspace filesystem daemon using the FUSE API, designed to read from and write to WAD files. Also utilizesa C++ library for parsing and manipulating WAD file headers, descriptors, and lump data, enabling efficient data handling and manipulation within the filesystem.",
-      languages: <><SiCoursera/><SiCplusplus /></>,
     },
     {
       title: "Sudoku Generator",
@@ -57,9 +65,18 @@ const Projects = () => {
       info: "Yes",
       images: [sudokuImg1, sudokuImg2],
       link: "https://github.com/jtaylor205/sudoku",
+      screen: "Computer",
       bio: "A Sudoku generator that creates boards based on chosen difficulty levels. Leveraging Pygame, users can sketch, delete, and submit answers as they attempt to solve the puzzle, providing an interactive and engaging experience.",
       languages: <SiPython />,
     },
+    {
+      title: "File System",
+      type: "Operating Systems",
+      info: "No",
+      bio: "A userspace filesystem daemon using the FUSE API, designed to read from and write to WAD files. Also utilizesa C++ library for parsing and manipulating WAD file headers, descriptors, and lump data, enabling efficient data handling and manipulation within the filesystem.",
+      languages: <><SiCoursera/><SiCplusplus /><SiLinux/></>,
+    },
+    
     {
       title: "Stock Market Trading Analyzer",
       type: "Data Analysis",
@@ -115,7 +132,7 @@ const Projects = () => {
             {selectedProject.images && (
               <div className='project-images'>
                 {selectedProject.images.map((image, index) => (
-                  <img key={index} src={image} alt={`${selectedProject.title} screenshot ${index + 1}`} className='project-image' />
+                  <img key={index} src={image} alt={`${selectedProject.title} screenshot ${index + 1}`} className= {selectedProject.screen == 'Mobile' ? 'project-image-mobile' : 'project-image-computer'} />
                 ))}
               </div>
             )}
